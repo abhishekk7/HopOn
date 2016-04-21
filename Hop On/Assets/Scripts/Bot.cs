db0f06f194f861a60eeb35484330130261cfbfab
abhishekk7 <abhishek3433kumar@gmail.com>
2016-04-21 18:01:59 -0500

    First Commit

---
+++ Hop On/Assets/Scripts/Bot.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bot : MonoBehaviour {
+
+    public float speed;
+    private Vector3 dir;
+
+	// Use this for initialization
+	void Start () {
+        dir = Vector3.zero;//To maintain bot stationary during start of game
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //To change bot direction based on keypress
+        if (Input.GetKey(KeyCode.LeftArrow))
+            dir = Vector3.back;
+        else if (Input.GetKey(KeyCode.RightArrow))
+            dir = Vector3.forward;
+
+        //To actually move bot
+        //Delta time is used to make the bot move identical in all devices. Frequency of update method call varies across devices
+        //Update call rate based on fps of device
+        float amountToMove = speed * Time.deltaTime;
+        transform.Translate(dir * amountToMove);
+
+        //dir = Vector3.up;
+        //transform.Translate(dir * amountToMove);
+
+        dir = Vector3.left;
+        transform.Translate(dir * amountToMove);
+        
+        //dir = Vector3.down;
+        //transform.Translate(dir * amountToMove);
+	}
+}
---
+++ Hop On/Assets/Scripts/TileManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileManager : MonoBehaviour {
+
+
+    public GameObject leftTilePrefab;
+    public GameObject rightTilePrefab;
+    public GameObject topTilePrefab;
+    public GameObject currentTile;
+	// Use this for initialization
+	void Start () {
+        
+            SpawnTile();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        SpawnTile();
+
+	}
+
+    public void SpawnTile()
+    {
+        
+        int nextTileDecider = Random.Range(1, 4);
+        switch(nextTileDecider)
+        {
+            case 1:
+                currentTile = (GameObject)Instantiate(leftTilePrefab, currentTile.transform.GetChild(0).transform.GetChild(0).position, Quaternion.identity);
+                break;
+            case 2:
+                currentTile = (GameObject)Instantiate(topTilePrefab, currentTile.transform.GetChild(0).transform.GetChild(2).position, Quaternion.identity);
+                break;
+            case 3:
+                currentTile = (GameObject)Instantiate(rightTilePrefab, currentTile.transform.GetChild(0).transform.GetChild(1).position, Quaternion.identity);
+                break;
+        }
+        
+    }
+}
